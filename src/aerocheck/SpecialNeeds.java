99a40ab17acc38597d6bf0f6fbd335a86167036d
joee <143257256+sjoee@users.noreply.github.com>
2024-06-14 19:10:32 +0800

    Update SpecialNeeds.java
    special needs file

--- src/aerocheck/SpecialNeeds.java
+++ src/aerocheck/SpecialNeeds.java
@@ -1,1 +1,28 @@
+package aerocheck;
 
+public class SpecialNeeds extends Passenger{
+    private String assistanceType;
+    
+    public SpecialNeeds(int bookingId, String fName, String lName, int gender, String contact, String nationality, String dob, String passportNo, String country, String passportExpiry, String issuingCountry, String assistanceType) {
+        super(bookingId, fName, lName, gender, contact, nationality, dob, passportNo, country, passportExpiry, issuingCountry, assistanceType);
+        this.assistanceType = assistanceType;
+    }
+
+    public String getAssistanceType(){
+        return assistanceType;
+    }
+
+    public void requestAssistance() {
+        // Logic to request special assistance
+        System.out.println(getFName() + " " + getLName() + " has requested assistance: " + assistanceType);
+    }
+
+    public void updateAssistanceDetails(String assistanceType) {
+        this.assistanceType = assistanceType;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nAssistance Type: " + assistanceType;
+    }
+}
