04783cd608459ff12266daecbfa4034a0f262bfe
Tterrence05 <142971374+Tterrence05@users.noreply.github.com>
2024-06-18 14:01:56 +0800

    Update Passenger.java

--- src/aerocheck/Passenger.java
+++ src/aerocheck/Passenger.java
@@ -1,122 +1,17 @@
-package aerocheck;
+package pcheckin;
 
-public class Passenger {
-    private int BookingID, gender;
-    private String FName, LName, email, contact, nationality, dob, passportNo,country,passportExpiry,issuingCountry;
-    
-    public Passenger(int BookingID, String FName, String LName, int gender, String email, String contact, String nationality, String dob, String passportNo, String country, String passportExpiry, String issuingCountry){
-        this.BookingID = BookingID;
-        this.gender = gender;
-        this.FName = FName;
-        this.LName = LName;
-        this.email = email;
-        this.contact = contact;
-        this.nationality = nationality;
-        this.dob = dob;
-        this.passportNo = passportNo;
-        this.country = country;
-        this.passportExpiry = passportExpiry;
-        this.issuingCountry = issuingCountry;
-    }
-    
-    public Passenger(){
-        
-    }
-    
-    public int getBookingID() {
-        return BookingID;
-    }
+import java.time.LocalDate;
 
-    public void setBookingID(int BookingID) {
-        this.BookingID = BookingID;
-    }
+public class PcheckIn {
+    public static void main(String[] args) {
+        CustomerInfo p1 = new CustomerInfo("P005", "Taylor Swift", "Cornelia Street", "Taylor1988@gmail.com", "123456", "A019988", LocalDate.of(2026, 5, 15), "USA");
+        CustomerInfo p2 = new CustomerInfo("P018", "Lana Del Rey", "Mariner Complex Apartment", "LanaDRey@gmail.com", "010203", "A11232", LocalDate.of(2025, 8, 20), "USA");
 
-    public int getGender() {
-        return gender;
-    }
+        BaggageInfo baggage1 = new BaggageInfo("B001", 18.50, p1.getCustomerID());
+        BaggageInfo baggage2 = new BaggageInfo("B002", 17.50, p2.getCustomerID());
 
-    public void setGender(int gender) {
-        this.gender = gender;
-    }
-
-    public String getFName() {
-        return FName;
-    }
-
-    public void setFName(String FName) {
-        this.FName = FName;
-    }
-
-    public String getLName() {
-        return LName;
-    }
-
-    public void setLName(String LName) {
-        this.LName = LName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getPassportNo() {
-        return passportNo;
-    }
-
-    public void setPassportNo(String passportNo) {
-        this.passportNo = passportNo;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPassportExpiry() {
-        return passportExpiry;
-    }
-
-    public void setPassportExpiry(String passportExpiry) {
-        this.passportExpiry = passportExpiry;
-    }
-
-    public String getIssuingCountry() {
-        return issuingCountry;
-    }
-
-    public void setIssuingCountry(String issuingCountry) {
-        this.issuingCountry = issuingCountry;
+        p1.selfCheckIn("38B", baggage1);
+        p2.groundHandlingCheckIn("18C", baggage2);
     }
 }
 
